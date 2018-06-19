8735103a5bcaf710c12fbef2bf20654eacd85f2c
yoyohoo <sinful_001@sina.com>
2018-06-19 15:53:58 +0800

    Update todo.module.js

--- modules/todo.module.js
+++ modules/todo.module.js
@@ -6,13 +6,31 @@ define(function (require, exports, module) {
         ser: require('../services/todo.service'),
         render: function () {
             var self = this;
-            this.ser.getTodo(function (data) {
-                self.el.empty();
-                $.each(data, (index, item) => {
-                    let tpl = `<li><a data-id="?uuid=${item.taskId}" href="#recheck" class="item-link item-content"><div class="item-inner"><div class="item-title-row"><div class="item-title">${item.taskName}</div> <div class="item-after">${item.submitDate}</div> </div><div class="item-text">${item.linkName}</div></div></a></li>`
-                    self.el.append(tpl);
-                })
-            });
+            var data = [{
+                taskId: 1,
+                taskName: '封闭性规则复核',
+                submitDate: '06-15 17:14',
+                linkName: '此处是待办事项展示内容...'
+            }, {
+                taskId: 2,
+                taskName: '封闭性规则复核2',
+                submitDate: '06-15 17:14',
+                linkName: '此处是待办事项展示内容...'
+            }];
+            self.el.empty();
+            $.each(data, (index, item) => {
+                // item.submitDate = item.submitDate.substr(0, 10)
+                let tpl = `<li><a data-id="?uuid=${item.taskId}" href="#recheck" class="item-link item-content"><div class="item-inner"><div class="item-title-row"><div class="item-title">${item.taskName}</div> <div class="item-after">${item.submitDate}</div> </div><div class="item-text">${item.linkName}</div></div></a></li>`
+                self.el.append(tpl);
+            })
+            // this.ser.getTodo(function (data) {
+            //     self.el.empty();
+            //     $.each(data, (index, item) => {
+            //         // item.submitDate = item.submitDate.substr(0, 10)
+            //         let tpl = `<li><a data-id="?uuid=${item.taskId}" href="#recheck" class="item-link item-content"><div class="item-inner"><div class="item-title-row"><div class="item-title">${item.taskName}</div> <div class="item-after">${item.submitDate}</div> </div><div class="item-text">${item.linkName}</div></div></a></li>`
+            //         self.el.append(tpl);
+            //     })
+            // });
         }
     }
 
